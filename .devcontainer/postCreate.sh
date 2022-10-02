c5e857e3134d0e9a136aed03a24ef8d8827d5036
Ronak Jain <ronakjain.public@gmail.com>
2022-10-02 08:38:54 +0530

    Update PAT secret with ADO in name

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,4 @@
-patEnvName="${adoOrg^^}_PAT"
+patEnvName="${adoOrg^^}_ADO_PAT"
 
 if [ ! -d "$sourceLocation" ]; then
     git clone "https://$adoOrg:${!patEnvName}@dev.azure.com/$adoOrg/$adoProject/_git/$adoRepo" $sourceLocation
